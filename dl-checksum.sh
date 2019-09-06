a656b288b3115349a99c307c97b66a4097b6eb91
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-09-05 21:39:35 -0500

    up to v19.03.2. start capturing docker-rootless-extras checksums.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,32 +1,40 @@
 #!/usr/bin/env sh
-VER='19.03.1'
+VER=${1:-19.03.2}
 DIR=~/Downloads
 MIRROR=https://download.docker.com
 
 dl()
 {
-    OS=$1
-    PLATFORM=$2
-    FILE=docker-$VER.tgz
-    URL=$MIRROR/$OS/static/stable/$PLATFORM/$FILE
-    LFILE=$DIR/docker-$OS-$PLATFORM-$VER.tgz
+    local app=$1
+    local os=$2
+    local arch=$3
+    local url=$MIRROR/$os/static/stable/$arch/$app-$VER.tgz
+    local lfile=$DIR/$app-$os-$arch-$VER.tgz
 
-    if [ ! -e $LFILE ];
+    if [ ! -e $lfile ];
     then
-        wget -q -O $LFILE $URL
+        wget -q -O $lfile $url
     fi
 
-    printf "      # %s\n" $URL
-    printf "      %s: sha256:%s\n" $PLATFORM `sha256sum $LFILE | awk '{print $1}'`
+    printf "      # %s\n" $url
+    printf "      %s: sha256:%s\n" $arch `sha256sum $lfile | awk '{print $1}'`
 }
 
+printf "docker_binaries_checksums:\n"
 printf "  '%s':\n" $VER
 printf "    %s:\n" linux
-dl linux aarch64
-dl linux armel
-dl linux armhf
-dl linux x86_64
+dl docker linux aarch64
+dl docker linux armel
+dl docker linux armhf
+dl docker linux x86_64
 printf "    %s:\n" mac
-dl mac x86_64
+dl docker mac x86_64
 
 
+printf "docker_rootless_extras_checksums:\n"
+printf "  '%s':\n" $VER
+printf "    %s:\n" linux
+dl docker-rootless-extras linux aarch64
+dl docker-rootless-extras linux armel
+dl docker-rootless-extras linux armhf
+dl docker-rootless-extras linux x86_64
