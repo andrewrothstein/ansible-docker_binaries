228ee458bcd9e3a8a71aeb740ad717e567b9d9ed
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-05-30 21:39:27 -0400

    up to v19.03.10

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
-VER=${1:-19.03.9}
 DIR=~/Downloads
 MIRROR=https://download.docker.com
 
 dl()
 {
-    local app=$1
-    local os=$2
-    local arch=$3
-    local url=$MIRROR/$os/static/stable/$arch/$app-$VER.tgz
-    local lfile=$DIR/$app-$os-$arch-$VER.tgz
+    local ver=$1
+    local app=$2
+    local os=$3
+    local arch=$4
+    local url=$MIRROR/$os/static/stable/$arch/$app-$ver.tgz
+    local lfile=$DIR/$app-$os-$arch-$ver.tgz
 
     if [ ! -e $lfile ];
     then
@@ -20,21 +20,26 @@ dl()
     printf "      %s: sha256:%s\n" $arch `sha256sum $lfile | awk '{print $1}'`
 }
 
-printf "docker_binaries_checksums:\n"
-printf "  '%s':\n" $VER
-printf "    %s:\n" linux
-dl docker linux aarch64
-dl docker linux armel
-dl docker linux armhf
-dl docker linux x86_64
-printf "    %s:\n" mac
-dl docker mac x86_64
+dl_ver () {
+    local ver=$1
 
+    printf "docker_binaries_checksums:\n"
+    printf "  '%s':\n" $ver
+    printf "    %s:\n" linux
+    dl $ver docker linux aarch64
+    dl $ver docker linux armel
+    dl $ver docker linux armhf
+    dl $ver docker linux x86_64
+    printf "    %s:\n" mac
+    dl $ver docker mac x86_64
 
-printf "docker_rootless_extras_checksums:\n"
-printf "  '%s':\n" $VER
-printf "    %s:\n" linux
-dl docker-rootless-extras linux aarch64
-dl docker-rootless-extras linux armel
-dl docker-rootless-extras linux armhf
-dl docker-rootless-extras linux x86_64
+    printf "docker_rootless_extras_checksums:\n"
+    printf "  '%s':\n" $ver
+    printf "    %s:\n" linux
+    dl $ver docker-rootless-extras linux aarch64
+    dl $ver docker-rootless-extras linux armel
+    dl $ver docker-rootless-extras linux armhf
+    dl $ver docker-rootless-extras linux x86_64
+}
+
+dl_ver ${1:-19.03.10}
