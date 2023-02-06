466a6960ff9e930542087aa70d71341c1618d530
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-02-05 21:41:08 -0500

    split out checksums

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -21,10 +21,9 @@ dl()
     printf "      %s: sha256:%s\n" $arch `sha256sum $lfile | awk '{print $1}'`
 }
 
-dl_ver () {
+dl_ver_root () {
     local ver=$1
 
-    printf "docker_binaries_checksums:\n"
     printf "  '%s':\n" $ver
     printf "    %s:\n" linux
     dl $ver docker linux aarch64
@@ -34,8 +33,11 @@ dl_ver () {
     printf "    %s:\n" mac
     dl $ver docker mac x86_64
     dl $ver docker mac aarch64
+}
 
-    printf "docker_rootless_extras_checksums:\n"
+dl_ver_rootless() {
+    local ver=$1
+
     printf "  '%s':\n" $ver
     printf "    %s:\n" linux
     dl $ver docker-rootless-extras linux aarch64
@@ -44,5 +46,15 @@ dl_ver () {
     dl $ver docker-rootless-extras linux x86_64
 }
 
-dl_ver 20.10.17
-dl_ver ${1:-20.10.18}
+dl_ver() {
+    local ver=$1
+    dl_ver_root $ver >> defaults/main/checksums.yml
+    dl_ver_rootless $ver >> defaults/main/rootless-checksums.yml
+}
+
+dl_ver 20.10.19
+#dl_ver 20.10.20
+#dl_ver 20.10.21
+#dl_ver 20.10.22
+#dl_ver 20.10.23
+#dl_ver ${1:-23.0.0}
